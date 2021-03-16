1bcf608e7348d20ffb404882abe1842c66e7a67b
xieqx <1374523006@qq.com>
2021-03-16 22:36:58 +0800

    创建节点提交

--- zookeeper/src/main/java/com/xiu/zookeeper/ZookeeperApplication.java
+++ zookeeper/src/main/java/com/xiu/zookeeper/ZookeeperApplication.java
@@ -1,6 +1,7 @@
 package com.xiu.zookeeper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
