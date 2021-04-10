43adfb9545e236e49cb30e205d9ba0d570cac97a
xieqixiu <xieqixiu@baoyinxiaofei.com>
2021-04-10 10:29:32 +0800

    zookeeper实现配置中心上传

---
+++ configcenter/src/test/java/com/xiu/zookeeper/configcenter/ConfigcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiu.zookeeper.configcenter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
